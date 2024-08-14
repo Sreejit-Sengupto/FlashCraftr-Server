0514a276a09b15b889547205041711bc64e0d44b
Sreejit-Sengupto <sreesen2003@gmail.com>
2024-08-14 21:33:41 +0530

    update cors with production url

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,8 @@ import cors from 'cors'
 const app = express()
 
 app.use(cors({
-    origin: 'https://flashcraftr.vercel.app'
+    origin: ['https://flashcraftr.vercel.app', 'http://localhost:5173', 'http://192.168.1.3:5173'],
+    // default: 'https://flashcraftr.vercel.app'
 }))
 app.use(express.static(path.join(__dirname, "client/dist")))
 app.use(express.json({ limit: '16kb'}))
